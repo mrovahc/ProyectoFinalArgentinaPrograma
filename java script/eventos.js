12af43b0fd9e049155f4095e5a54e4922016a9bf
Lucas Talia <talial@cpe.com>
2023-01-19 10:42:57 -0300

    Se agrega el Proyecto

---
+++ java script/eventos.js
@@ -0,0 +1,45 @@
+function enAmarillo1() {
+Element =document.getElementById("pers");
+Element.src="img/persona5.png";
+}
+function enNegro1() {
+    Element =document.getElementById("pers");
+    Element.src="img/persona4.png";
+}
+
+function enAmarillo2() {
+    Element =document.getElementById("labo");
+    Element.src="img/valija5.png";
+}
+ function enNegro2() {
+    Element =document.getElementById("labo");
+    Element.src="img/valija4.jpg";
+}
+
+function enAmarillo3() {
+    Element =document.getElementById("Cono");
+    Element.src="img/habilidades4.jpg";
+}
+function enNegro3() {
+    Element =document.getElementById("Cono");
+    Element.src="img/habilidades3.jfif";
+}
+
+function enAmarillo4() {
+    Element =document.getElementById("estu");
+    Element.src="img/Estudios4.jpg";
+}
+function enNegro4() {
+    Element =document.getElementById("estu");
+    Element.src="img/Estudios3.jpg";
+}
+
+
+function enAmarillo5() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin2.png";
+}
+function enNegro5() {
+    Element =document.getElementById("li");
+    Element.src="img/linkedin.png";
+}
